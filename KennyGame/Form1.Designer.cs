a1b947bbd850566860ec47bd8eba2b1194ab5208
ahmettinastepe <ahmettinastepe0@gmail.com>
2020-03-18 19:36:27 +0300

    Add project files.

---
+++ KennyGame/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace KennyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kenny1 = new System.Windows.Forms.PictureBox();
+            this.kenny2 = new System.Windows.Forms.PictureBox();
+            this.kenny3 = new System.Windows.Forms.PictureBox();
+            this.kenny6 = new System.Windows.Forms.PictureBox();
+            this.kenny5 = new System.Windows.Forms.PictureBox();
+            this.kenny4 = new System.Windows.Forms.PictureBox();
+            this.kenny9 = new System.Windows.Forms.PictureBox();
+            this.kenny8 = new System.Windows.Forms.PictureBox();
+            this.kenny7 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timerBoard = new System.Windows.Forms.Timer(this.components);
+            this.timerKenny = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.kenny1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kenny1
+            // 
+            this.kenny1.Image = ((System.Drawing.Image)(resources.GetObject("kenny1.Image")));
+            this.kenny1.Location = new System.Drawing.Point(12, 145);
+            this.kenny1.Name = "kenny1";
+            this.kenny1.Size = new System.Drawing.Size(100, 101);
+            this.kenny1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny1.TabIndex = 0;
+            this.kenny1.TabStop = false;
+            this.kenny1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny2
+            // 
+            this.kenny2.Image = ((System.Drawing.Image)(resources.GetObject("kenny2.Image")));
+            this.kenny2.Location = new System.Drawing.Point(143, 145);
+            this.kenny2.Name = "kenny2";
+            this.kenny2.Size = new System.Drawing.Size(100, 101);
+            this.kenny2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny2.TabIndex = 1;
+            this.kenny2.TabStop = false;
+            this.kenny2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny3
+            // 
+            this.kenny3.Image = ((System.Drawing.Image)(resources.GetObject("kenny3.Image")));
+            this.kenny3.Location = new System.Drawing.Point(274, 145);
+            this.kenny3.Name = "kenny3";
+            this.kenny3.Size = new System.Drawing.Size(100, 101);
+            this.kenny3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny3.TabIndex = 2;
+            this.kenny3.TabStop = false;
+            this.kenny3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny6
+            // 
+            this.kenny6.Image = ((System.Drawing.Image)(resources.GetObject("kenny6.Image")));
+            this.kenny6.Location = new System.Drawing.Point(274, 273);
+            this.kenny6.Name = "kenny6";
+            this.kenny6.Size = new System.Drawing.Size(100, 101);
+            this.kenny6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny6.TabIndex = 5;
+            this.kenny6.TabStop = false;
+            this.kenny6.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny5
+            // 
+            this.kenny5.Image = ((System.Drawing.Image)(resources.GetObject("kenny5.Image")));
+            this.kenny5.Location = new System.Drawing.Point(143, 273);
+            this.kenny5.Name = "kenny5";
+            this.kenny5.Size = new System.Drawing.Size(100, 101);
+            this.kenny5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny5.TabIndex = 4;
+            this.kenny5.TabStop = false;
+            this.kenny5.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny4
+            // 
+            this.kenny4.Image = ((System.Drawing.Image)(resources.GetObject("kenny4.Image")));
+            this.kenny4.Location = new System.Drawing.Point(12, 273);
+            this.kenny4.Name = "kenny4";
+            this.kenny4.Size = new System.Drawing.Size(100, 101);
+            this.kenny4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny4.TabIndex = 3;
+            this.kenny4.TabStop = false;
+            this.kenny4.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny9
+            // 
+            this.kenny9.Image = ((System.Drawing.Image)(resources.GetObject("kenny9.Image")));
+            this.kenny9.Location = new System.Drawing.Point(274, 399);
+            this.kenny9.Name = "kenny9";
+            this.kenny9.Size = new System.Drawing.Size(100, 101);
+            this.kenny9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny9.TabIndex = 8;
+            this.kenny9.TabStop = false;
+            this.kenny9.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny8
+            // 
+            this.kenny8.Image = ((System.Drawing.Image)(resources.GetObject("kenny8.Image")));
+            this.kenny8.Location = new System.Drawing.Point(143, 399);
+            this.kenny8.Name = "kenny8";
+            this.kenny8.Size = new System.Drawing.Size(100, 101);
+            this.kenny8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny8.TabIndex = 7;
+            this.kenny8.TabStop = false;
+            this.kenny8.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // kenny7
+            // 
+            this.kenny7.Image = ((System.Drawing.Image)(resources.GetObject("kenny7.Image")));
+            this.kenny7.Location = new System.Drawing.Point(12, 399);
+            this.kenny7.Name = "kenny7";
+            this.kenny7.Size = new System.Drawing.Size(100, 101);
+            this.kenny7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kenny7.TabIndex = 6;
+            this.kenny7.TabStop = false;
+            this.kenny7.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Good Times Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 556);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Skor : ";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Good Times Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSkor.Location = new System.Drawing.Point(189, 556);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 23);
+            this.lblSkor.TabIndex = 11;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Roboto Bk", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTimer.Location = new System.Drawing.Point(161, 32);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(81, 58);
+            this.lblTimer.TabIndex = 12;
+            this.lblTimer.Text = "30";
+            // 
+            // timerBoard
+            // 
+            this.timerBoard.Interval = 1000;
+            this.timerBoard.Tick += new System.EventHandler(this.timerBoard_Tick);
+            // 
+            // timerKenny
+            // 
+            this.timerKenny.Interval = 500;
+            this.timerKenny.Tick += new System.EventHandler(this.timerKenny_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(388, 642);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kenny9);
+            this.Controls.Add(this.kenny8);
+            this.Controls.Add(this.kenny7);
+            this.Controls.Add(this.kenny6);
+            this.Controls.Add(this.kenny5);
+            this.Controls.Add(this.kenny4);
+            this.Controls.Add(this.kenny3);
+            this.Controls.Add(this.kenny2);
+            this.Controls.Add(this.kenny1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kenny Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kenny1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kenny7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kenny1;
+        private System.Windows.Forms.PictureBox kenny2;
+        private System.Windows.Forms.PictureBox kenny3;
+        private System.Windows.Forms.PictureBox kenny6;
+        private System.Windows.Forms.PictureBox kenny5;
+        private System.Windows.Forms.PictureBox kenny4;
+        private System.Windows.Forms.PictureBox kenny9;
+        private System.Windows.Forms.PictureBox kenny8;
+        private System.Windows.Forms.PictureBox kenny7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timerBoard;
+        private System.Windows.Forms.Timer timerKenny;
+    }
+}
+
---
+++ KennyGame/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KennyGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private int skor = 0;
+        private int timer = 30;
+        Random random = new Random();
+
+        void HideKenny()
+        {
+            PictureBox[] kennyArray = { kenny1, kenny2, kenny3, kenny4, kenny5, kenny6, kenny7, kenny8, kenny9 };
+
+            foreach (var kenny in kennyArray)
+            {
+                kenny.Visible = false;
+            }
+
+            var kennyShow = random.Next(0, kennyArray.Length);
+            kennyArray[kennyShow].Visible = true;
+        }
+
+        void TimerDown()
+        {
+            timerBoard.Enabled = true;
+            timerBoard.Start();
+            timerKenny.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimerDown();
+            HideKenny();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            skor += 1;
+            lblSkor.Text = skor.ToString();
+        }
+        private void timerBoard_Tick(object sender, EventArgs e)
+        {
+            timer--;
+            lblTimer.Text = timer.ToString();
+
+            if (timer == 0)
+            {
+                timerBoard.Stop();
+                timerKenny.Stop();
+                DialogResult mesaj = MessageBox.Show("Zaman Doldu", "Zamanınız Doldu. Tekrar oynamak ister misiniz?",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (mesaj == DialogResult.Yes)
+                {
+                    TimerDown();
+                    HideKenny();
+                    skor = 0;
+                    timer = 31;
+                }
+            }
+        }
+        private void timerKenny_Tick(object sender, EventArgs e)
+        {
+            HideKenny();
+        }
+    }
+}
